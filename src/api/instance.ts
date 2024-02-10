783a0f32bade6effb7c0baf1a92f539727ba8d6e
georgievdd <akaden2017@mail.ru>
2024-02-11 02:39:13 +0300

    lit fix

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const HOST = process.env.REACT_APP_BACKEND_HOST
 const PORT = process.env.REACT_APP_BACKEND_PORT
-const apiUrl = `${HOST}:${PORT}/api/v1`
+const apiUrl = `http://${HOST}:${PORT}/api/v1`
 
 export const axiosInstance = axios.create({
   baseURL: apiUrl,

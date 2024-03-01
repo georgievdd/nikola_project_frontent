3afe88b8d28a1edf7c39d493a9797e064ec3e33f
georgievdd <akaden2017@mail.ru>
2024-03-02 02:16:34 +0300

    swiper refact

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { Swiper as LibSwiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y, EffectFade } from 'swiper/modules';
+import { Navigation, Pagination, A11y, EffectFade, Scrollbar } from 'swiper/modules';
 import styles from './Swiper.module.scss'
 import { Picture } from '@/entity/House';
-import Image from 'next/image';
 import { STATIC_URL } from '@/api/instance';
 
 import 'swiper/swiper-bundle.css'
@@ -21,9 +20,10 @@ export default function Swiper(props: SwiperProps) {
       <LibSwiper
         rewind={true}
         navigation
+        slidesPerView={1}
         effect={'fade'}
         pagination={{clickable: true}}
-        modules={[Navigation, Pagination, A11y, EffectFade]}
+        modules={[Navigation, EffectFade, Pagination/*Scrollbar, A11y*/]}
         className={[styles.swiper, props?.className].join(' ')}
       >
         {props.links.map(link => (
@@ -31,7 +31,8 @@ export default function Swiper(props: SwiperProps) {
             <img 
               src={`${STATIC_URL}${link.picture_path}`} 
               // fill 
-              alt=''/>
+              alt=''
+            />
           </SwiperSlide>
         ))}
       </LibSwiper>

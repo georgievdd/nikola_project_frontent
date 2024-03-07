f57b0b95bc2e641f4eead4bb41337c79df2c72b4
georgievdd <akaden2017@mail.ru>
2024-03-08 02:54:11 +0300

    fix backend's update 2

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,3 +1,1 @@
-export const HOST = process.env.BACKEND_HOST//process.env.NEXT_PUBLIC_BACKEND_HOST
-export const PORT = process.env.BACKEND_PORT//process.env.NEXT_PUBLIC_BACKEND_PORT
-export const API_URL = `${HOST}:${PORT}/backend/api/v1`
+export const API_URL = `${process.env.BACKEND_URL}/api/v1`

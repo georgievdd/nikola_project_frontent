6108d7aed385afad8f125c531aa7459358099128
georgievdd <akaden2017@mail.ru>
2024-03-21 02:19:29 +0300

    try to fix page-id with query

--- src/app/house/[id]/getStaticPaths.ts
+++ src/app/house/[id]/getStaticPaths.ts
@@ -1,7 +1,7 @@
-import { GetStaticPaths } from 'next'
+// import { GetStaticPaths } from 'next'
 
-export const getStaticPaths: GetStaticPaths = async () => {
-    const ids = ['1', '2', '3'];
-    const paths = ids.map(id => ({ params: { id } }));
-    return { paths, fallback: true };
-  };
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const ids = ['1', '2', '3'];
+//   const paths = ids.map(id => ({ params: { id } }));
+//   return { paths, fallback: true };
+// };

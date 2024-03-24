f0cd881c45b746fe6b432796d8af6f91b60dd65a
georgievdd <akaden2017@mail.ru>
2024-03-24 20:07:44 +0300

    some fix

--- src/app/house/page.tsx
+++ src/app/house/page.tsx
@@ -1,13 +1,30 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import HousesHolder from "@/components/HousesHolder/HousesHolder";
 import { getHouses } from "@/api/house";
 import { House } from '@/entity/House';
+import Logo from '../../../public/images/logo.svg'
 
 export const metadata: Metadata = {
   title: "Nikola | Houses",
   description: "houses to book",
   keywords: "nikola, house"
+}
+
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const houses = await getHouses()
+  const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title:  `Nikola | Houses`,
+    description: "Забранируйте домик для отдыха, ради христа",
+    keywords: "nikola, houses, ",
+    openGraph: {
+      images: [Logo, ...previousImages],
+    },
+  }
 }
 
 const HousesPage = async () => {

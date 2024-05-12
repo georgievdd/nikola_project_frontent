b89c3ab1261e05c21b73b5900ea3aa710279dac8
georgievdd <akaden1017@mail.ru>
2024-05-12 17:25:21 +0300

    added dif env for service

--- src/components/ServicePage/static_data.ts
+++ src/components/ServicePage/static_data.ts
@@ -10,7 +10,9 @@ export interface Service {
     measurement: string
 }
 
-const tgLink: string = process.env.NEXT_PUBLIC_SERVICE_TG_LINK || ''
+const tgLinkSupboard: string = process.env.NEXT_PUBLIC_SERVICE_TG_LINK_SUPBOARD!
+const tgLinkBathhouse: string = process.env.NEXT_PUBLIC_SERVICE_TG_LINK_BATHHOUSE!
+const tgLinkGrill: string = process.env.NEXT_PUBLIC_SERVICE_TG_LINK_GRILL!
 
 const genImgLinks = (prefix: string, length: number) => 
     Array.from({length}).map((_, i) => `${prefix}/${i + 1}.png`)
@@ -22,7 +24,7 @@ export const serviceData: Service[] = [
             'Отдых в русской бане - это великолепное путешествие в мир, где встречаются вековые традиции и естественные методы оздоровления.',
             'Это не просто место для процедур и расслабления, это истинное святилище, где каждый уголок наполнен атмосферой тепла, заботы и природной гармонии. В нашей русской бане вы ощутите, как древние традиции сливаются с современным комфортом.',
         ],
-        tgLink,
+        tgLink: tgLinkBathhouse,
         cost: '5000',
         measurement: '₽/час',
         imgUrls: genImgLinks('bathhouse', 3),
@@ -33,7 +35,7 @@ export const serviceData: Service[] = [
             'Аренда сапов - это увлекательное приключение на свежем воздухе, приносящее радость и вдохновение. Взять в аренду сапы - значит погрузиться в мир водных просторов и природной гармонии.',
             'Это не просто способ провести время, а возможность познакомиться с водными путешествиями в новом формате. Здесь каждый может почувствовать себя исследователем, преодолевая водные просторы на доске. ',
         ],
-        tgLink,
+        tgLink: tgLinkSupboard,
         cost: '3000',
         measurement: '₽/час',
         imgUrls: genImgLinks('sapboard', 3),
@@ -44,7 +46,7 @@ export const serviceData: Service[] = [
             'Аренда мангалов - это возможность провести незабываемое время на природе в кругу семьи и друзей, наслаждаясь вкусом свежей пищи, ароматом дымка и теплом огня.',
             'Это не просто аренда оборудования для приготовления барбекю, это создание атмосферы уюта и веселья на пикнике или празднике. Здесь каждый может почувствовать себя шеф-поваром и гриль-мастером, готовя вкусные блюда под открытым небом.',
         ],
-        tgLink,
+        tgLink: tgLinkGrill,
         cost: '5000',
         measurement: '₽/день',
         imgUrls: genImgLinks('grill', 3),

0718fc6b264406f16f7f0ca71b5d1427cb2b1180
georgievdd <akaden2017@mail.ru>
2024-07-14 15:53:48 +0300

    fixes get

--- src/api/service/index.ts
+++ src/api/service/index.ts
@@ -2,6 +2,12 @@ import { Service } from "@/entity/Service";
 import { FETCH_SERVICES } from "../endpoints";
 
 export async function getServices(): Promise<Service[]> {
-    return fetch(FETCH_SERVICES)
+    return fetch(FETCH_SERVICES, {
+            next: {
+                revalidate: 10
+            },
+            method: 'GET',
+        })
         .then(res => res.json())
+        .catch(e => [])
 }

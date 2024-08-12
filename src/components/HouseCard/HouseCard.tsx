ec4547f010004e99449f0161d8cd1db7dbea80a9
georgievdd <akaden2017@mail.ru>
2024-08-12 22:12:13 +0300

    eddited house cards

--- src/components/HouseCard/HouseCard.tsx
+++ src/components/HouseCard/HouseCard.tsx
@@ -25,6 +25,18 @@ const HouseCard = ({
   selectionController: SelectionController
   guestsController: INumberInput
 }) => {
+  const {
+    id,
+    base_persons_amount,
+    max_persons_amount,
+    total_price,
+    description,
+    name,
+    features,
+    pictures,
+    base_price,
+  } = data
+
   const buttonOnClick = () => {
     if (selectionController.isActive) {
       sessionStorage.setItem(
@@ -40,42 +52,42 @@ const HouseCard = ({
   }
 
   const getCostText = () => {
-    if (data.total_price) {
+    if (total_price) {
       const diff = getTimeDiff(
         selectionController.dateEnd!,
         selectionController.dateBegin!,
       )
-      return `${data.total_price} ₽ за ${diff} ${getNightsDeclension(diff)}`
+      return `${total_price} ₽ за ${diff} ${getNightsDeclension(diff)}`
     }
-    return `от ${data.base_price} ₽ за ночь`
+    return `от ${base_price} ₽ за ночь`
   }
 
   return (
     <div className={styles.container}>
-      <Swiper className={styles.preview} links={data.pictures.slice(0, 8)} />
+      <Swiper className={styles.preview} links={pictures.slice(0, 8)} />
       <article>
         <header>
-          <h1>{data.name}</h1>
+          <h1>{name}</h1>
         </header>
         <div className={styles['description-group']}>
-          <p className={styles.description}>{data.description}</p>
+          <p className={styles.description}>{description}</p>
           <div className={styles.features}>
             <FeatureList
               features={[
                 {
-                  name: `${data.base_persons_amount} без доплаты`,
+                  name: `${base_persons_amount} - ${max_persons_amount}`,
                   picture: BasePersonsImage,
                   id: 0,
                   withoutPrefix: true,
                 },
-                ...data.features,
+                ...features,
               ]}
               limit={4}
             />
           </div>
         </div>
         <div className={styles['bottom-group']}>
-          <Link href={`house/${data.id}`}>
+          <Link href={`house/${id}`}>
             <Button onClick={buttonOnClick}>Забронировать</Button>
           </Link>
           <p className={styles.cost}>{getCostText()}</p>
--- src/components/HouseHolder/Blocks/HouseDescriptionBlock/HouseDescriptionBlock.tsx
+++ src/components/HouseHolder/Blocks/HouseDescriptionBlock/HouseDescriptionBlock.tsx
@@ -3,34 +3,44 @@ import React from 'react'
 import Carousel from 'components/ui/Carousel/Carousel'
 import FeatureList from 'components/ui/FeatureList/FeatureList'
 import {House, HouseOptions} from 'entity/House'
+import AddPersonImage from 'images/add-person.svg'
 import BasePersonsImage from 'images/persons.svg'
 
-import styles from './HouseDescriptionBlock.module.scss'
+const css = require('src/helpers').importStyles(
+  require('./HouseDescriptionBlock.module.scss'),
+)
+
 interface HouseDescriptionBlock {
   data: House
   houseOptions: HouseOptions
 }
 const HouseDescriptionBlock = ({data, houseOptions}: HouseDescriptionBlock) => {
   return (
-    <div className={styles.container}>
+    <div className={css`container`}>
       <header>
         <h1>{data.name}</h1>
       </header>
       <section>
         <Carousel imgs={data.pictures} />
-        <div className={styles.features}>
+        <div className={css`feature-group`}>
           <FeatureList
             features={[
-              ...data.features,
               {
                 name: `${houseOptions.base_persons_amount} гостя без доплаты`,
                 picture: BasePersonsImage,
                 id: 0,
                 withoutPrefix: true,
               },
+              {
+                name: `${houseOptions.price_per_extra_person} ₽/день`,
+                picture: AddPersonImage,
+                id: 0,
+                withoutPrefix: true,
+              },
             ]}
-            className={styles.features}
+            className={css`features`}
           />
+          <FeatureList features={data.features} className={css`features`} />
         </div>
       </section>
       <article>{data.description}</article>

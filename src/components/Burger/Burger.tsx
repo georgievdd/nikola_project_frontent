041e632140b85bc7e574a403d2613df34fdb2b26
georgievdd <akaden2017@mail.ru>
2024-09-01 18:36:30 +0300

    burger fix

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,33 +1,36 @@
 'use client'
 import styles from './Burger.module.scss'
 
-const css = require('src/helpers').importStyles('./Burger.module.scss')
-
 interface BurgerProps {
   sidebarOpen: boolean
   setSidebarOpen: (arg: boolean) => void
 }
 
+const css = (str: string) =>
+  str
+    .split(' ')
+    .map((s) => styles[s])
+    .join(' ')
+
 const Burger = ({sidebarOpen, setSidebarOpen}: BurgerProps) => {
   const toggleClass = () => {
     setSidebarOpen(!sidebarOpen)
   }
-
   return (
     <div
       style={{zIndex: 10000}}
       onClick={toggleClass}
-      className={css`ham hamRotate ham1 ${sidebarOpen ? styles.active : ''}`}
+      className={css(`ham hamRotate ham1 ${sidebarOpen ? styles.active : ''}`)}
     >
       <svg viewBox="0 0 100 100" width="55">
         <path
-          className={css`line top`}
+          className={`${styles.line} ${styles.top}`}
           // eslint-disable-next-line max-len
           d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
         />
-        <path className={css`line middle`} d="m 30,50 h 40" />
+        <path className={`${styles.line} ${styles.middle}`} d="m 30,50 h 40" />
         <path
-          className={css`line bottom`}
+          className={`${styles.line} ${styles.bottom}`}
           // eslint-disable-next-line max-len
           d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40"
         />
